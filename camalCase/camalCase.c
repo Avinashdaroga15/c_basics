2a4be57d369fee96fd07794f4329c1eea062e528
Avinashdaroga15 <19embit013@mlvti.ac.in>
2022-07-11 15:07:38 +0530

    Camal case logic complete

--- camalCase/camalCase.c
+++ camalCase/camalCase.c
@@ -7,12 +7,33 @@ int main(){
 
     fptr = fopen("string.txt","r");
     string[i]=fgetc(fptr);
+    //entering string into an 1-d array
     while (string[i]!= EOF)
     {
         i++;
         string[i]=fgetc(fptr);
         
     }
+    //printing read string from file
+    i=0;
+    while (string[i]!= '.')
+    {
+        printf("%c",string[i]);
+        i++;
+    }
+    //changing case of each words first character 
+    i=0;
+    string[i] = string[i]-32;
+    while (string[i]!= '.')
+    {
+        i++;
+        if (string[i]==' ')
+        {
+            string[i+1]= string[i+1]-32;
+        }
+        
+    }
+    //printing string after converting in Camal case
     i=0;
     while (string[i]!= '.')
     {
